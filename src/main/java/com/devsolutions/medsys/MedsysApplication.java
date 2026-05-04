2d19b7be2201830f06871e0aab9cbc95f7d5ccc8
Swetony <swetonyancelmo@gmail.com>
2026-05-03 21:43:44 -0300

    chore: initial commit

---
+++ src/main/java/com/devsolutions/medsys/MedsysApplication.java
@@ -0,0 +1,13 @@
+package com.devsolutions.medsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsolutions/medsys/MedsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsolutions.medsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
