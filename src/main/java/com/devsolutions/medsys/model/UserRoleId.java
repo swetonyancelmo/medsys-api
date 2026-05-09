94285b1b15b6c1b7b8c6142d8b2a9da2d4767ab9
Swetony <swetonyancelmo@gmail.com>
2026-05-09 00:59:38 -0300

    fix: implement equal and hashCode for UserRoleId

--- src/main/java/com/devsolutions/medsys/model/UserRoleId.java
+++ src/main/java/com/devsolutions/medsys/model/UserRoleId.java
@@ -2,11 +2,17 @@ package com.devsolutions.medsys.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.UUID;
 
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class UserRoleId implements Serializable {
 
     @Column(name = "usuario_id")
