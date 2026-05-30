6132e887e5c8ff5550841fc8dbab35ff6ece27ef
David Lima <davidlimma05@gmail.com>
2026-05-30 12:03:01 -0300

    Ajustes no teste controller, foi apagado devido a inconsistencias pontuais, começará a ser feito novamente

--- src/test/java/com/devsolutions/medsys/controller/AppointmentControllerTest.java
+++ src/test/java/com/devsolutions/medsys/controller/AppointmentControllerTest.java
@@ -1,19 +1,14 @@
 package com.devsolutions.medsys.controller;
 
-import com.devsolutions.medsys.config.security.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(AppointmentController.class)
 @AutoConfigureMockMvc(addFilters = false)
 class AppointmentControllerTest {
-
-    @MockitoBean
-    private JwtUtil jwtUtil;
 
     @Test
     void schedule() {
